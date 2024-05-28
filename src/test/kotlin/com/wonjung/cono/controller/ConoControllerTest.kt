4a394965a847595269c529668b39054bf78523a7
jungyeunwon <wjyddd@naver.com>
2024-05-28 14:12:09 +0900

    test #4 - 노래방 목록 조회 컨트롤러 응답 테스트
    - GET /api/conos API 테스트 코드 작성
    - 테스트 관련 설정 추가

---
+++ src/test/kotlin/com/wonjung/cono/controller/ConoControllerTest.kt
@@ -0,0 +1,35 @@
+package com.wonjung.cono.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class ConoControllerTest(
+    @Autowired val mockMvc: MockMvc,
+    @Autowired val objectMapper: ObjectMapper
+) {
+    @Test
+    fun getConoList() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/conos")
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.conos[0].id").value(1))
+            .andExpect(jsonPath("$.conos[0].name").value("판타스틱 코인노래방 미금역점"))
+            .andExpect(jsonPath("$.conos[0].address").value("성남시 분당구 XX-XX"))
+            .andExpect(jsonPath("$.conos[1].id").value(2))
+            .andExpect(jsonPath("$.conos[1].name").value("슈퍼스타 코인노래방 미금역점"))
+            .andExpect(jsonPath("$.conos[1].address").value("성남시 분당구 XX-XX"))
+            .andDo(print())
+    }
+}
